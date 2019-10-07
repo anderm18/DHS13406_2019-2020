c38767e1c95a4d65c0a6c91e668844b125373fd7
Michael Anderson <michael.j.anderson@me.com>
2019-10-06 21:10:00 -0400

    Changes to Drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -1,9 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 //package is where your class is stored
 
+import android.os.UserManager;
+
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 //import just imports other classes aka other chunks of code that already have commands/methods written so you dont have to write them
 
 @TeleOp(name="Drive", group="1")
@@ -14,6 +18,9 @@ public class Drive extends OpMode
     private DcMotor rightFront = null;
     private DcMotor leftBack = null;
     private DcMotor rightBack = null;
+    private DcMotor leftIntake = null;
+    private DcMotor rightIntake = null;
+    private Servo grabber = null;
 
 
     //Once the START Button is pressed
@@ -25,8 +32,15 @@ public class Drive extends OpMode
         //define what hardware is being used on your robot and in your code (break the barrier between hardware and software
         leftFront  = hardwareMap.get(DcMotor.class, "leftFront");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-        leftBack = hardwareMap.get(DcMotor.class, "leftback");
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        leftIntake = hardwareMap.get(DcMotor.class, "leftIntake");
+        rightIntake = hardwareMap.get(DcMotor.class, "rightIntake");
+
+        grabber = hardwareMap.get(Servo.class, "grabber");
+
+
 
 
         //set diretion of motors (if left and right always turn clockwise, then the robot will spin, not good)
@@ -35,6 +49,8 @@ public class Drive extends OpMode
         leftBack.setDirection(DcMotor.Direction.FORWARD);
         rightFront.setDirection(DcMotor.Direction.REVERSE);
 
+        leftIntake.setDirection(DcMotor.Direction.FORWARD);
+        rightIntake.setDirection(DcMotor.Direction.REVERSE);
     }
 
     //Code that Loops after the initialize button is pressed (generally don't touch)
@@ -45,28 +61,43 @@ public class Drive extends OpMode
     //Code that runs once after the start button is pressed (generally also don't touch this)
     @Override
     public void start() {
+
+        grabber.resetDeviceConfigurationForOpMode();
     }
 
     //Code that Loops (runs continuously)
     @Override
     public void loop() {
 
+        if (gamepad2.x == true){
+
+            grabber.setPosition(Servo.MAX_POSITION);
+        }
+
+        if (gamepad2.b == true){
+
+            grabber.setPosition(Servo.MIN_POSITION);
+        }
+
+        leftIntake.setPower(gamepad2.left_trigger);
+        rightIntake.setPower(gamepad2.right_trigger);
+
         //It gets a little complex here, as we get into to Trigonometry
         double hyp = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
         //Above finds the Hypotenuse (the long side of a triangle) of the values of the x joystick and y joystick.
         //The Joysticks are set up on an xy plane, meaning vertical gives a y value and horizontal gives an x value
-        double rAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        double rAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
         //Above is a trig idenitity. Basically, all you need to know is that it's using a mathematical law to solve the angle (theta) the robot is at with the x axis
-        double turn = gamepad1.right_stick_x;
+        double turn = -gamepad1.right_stick_x;
         //Above is basically just turning, when you move x left the robot turns left, when right it turns right. No Mecanum drive in there.
 
         final double lF = hyp * Math.cos(rAngle) + turn;
         //Cosine (cos) is taking the angle the robot is facing (defined above), and finding the hypotanuse and adding turn (how far the x on the joystick is moved aka the left right thing)
         final double rF = hyp * Math.sin(rAngle) - turn;
         //same as cos except using the sine (sin) function. It's doing the same thing, but different math due to different angle
-        final double lB = hyp * Math.cos(rAngle) + turn;
+        final double lB = hyp * Math.sin(rAngle) + turn;
         //same as cos
-        final double rB = hyp * Math.sin(rAngle) - turn;
+        final double rB = hyp * Math.cos(rAngle) - turn;
         //same as sin
 
 
